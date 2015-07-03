737d1a9fd945abd1bfdfd4bc484c49760584275e
Vicente <vicente.merlo@ufrgs.br>
2015-07-02 23:42:49 -0300

    remover cloudflare so you wont be flagged as an attacker

--- src/content.js
+++ src/content.js
@@ -1,3 +1,3 @@
-if (document.body.innerHTML == 'The service is unavailable.' || document.title == 'Website is offline | 524: A timeout occurred' || document.title == 'Service Unavailable') {
+if (document.body.innerHTML == 'The service is unavailable.' || document.title == 'Service Unavailable') {
 	location.reload();
 }
