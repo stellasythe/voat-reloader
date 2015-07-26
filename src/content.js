9391481552a373116969fe7e3ec618545f1d6aaf
stellasythe <stellasythe@users.noreply.github.com>
2015-07-26 16:47:52 -0300

    Works on Reddit now, fixed minor issue
    Direct comparison with string literal wasn't returning true because innerHTML returns encoded HTML. 
    
    It's working on Reddit too now.

--- src/content.js
+++ src/content.js
@@ -1,3 +1,4 @@
-if (document.body.innerHTML == 'The service is unavailable.' || document.title == 'Service Unavailable') {
+if (document.body.innerHTML.search("The service is unavailable") || document.title == 'Service Unavailable' || document.body.innerHTML.search("All of our servers are busy right now")) {
 	location.reload();
 }
+
